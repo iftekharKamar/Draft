c1444e8ad44d3714cc864c2f762f2fd93a88e433
iftekharKamar <iftekharkamar@gmail.com>
2025-11-22 21:19:01 +0530

    adding dotenv

--- middlewares/multerMiddleware.js
+++ middlewares/multerMiddleware.js
@@ -3,6 +3,8 @@ const cloudinary = require("cloudinary").v2;
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
 const multer=require('multer')
 
+require('dotenv').config()
+
 
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
